0b3ba01c4aa9cf3f755e334f660b0af40fd04aac
Nuriya Akhmedshina <nyriya36@gmail.com>
2024-03-29 15:41:17 +0500

    ShopItemActivity.kt changed

--- app/src/main/java/com/selva/shoppinglist20/presentation/MainActivity.kt
+++ app/src/main/java/com/selva/shoppinglist20/presentation/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.selva.shoppinglist20.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.selva.shoppinglist20.R
-import com.selva.shoppinglist20.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
+        }
+        val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
+        buttonAddItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
     }
 
@@ -69,6 +75,8 @@ class MainActivity : AppCompatActivity() {
     private fun setupClickListener() {
         shopListAdapter.onShopItemClickListener = {
             Log.d("MainActivity", it.toString())
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
         }
     }
 
