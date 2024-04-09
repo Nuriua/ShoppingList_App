008d6bf7db637a706b327e33d76cdc71b8b5bdd5
Nuriya Akhmedshina <nyriya36@gmail.com>
2024-04-09 14:27:24 +0500

    databinding added

--- app/src/main/java/com/selva/shoppinglist20/domain/DeleteShopItemUseCase.kt
+++ app/src/main/java/com/selva/shoppinglist20/domain/DeleteShopItemUseCase.kt
@@ -2,7 +2,7 @@ package com.selva.shoppinglist20.domain
 
 class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun deleteShopItem(shopItem: ShopItem){
+    suspend fun deleteShopItem(shopItem: ShopItem){
         shopListRepository.deleteShopItem(shopItem)
     }
 }
